f9ad35e0344d56b1ac889395398eea92e8670fdb
Subham8989 <srigurujay1@gmail.com>
2025-11-28 09:42:20 +0530

    Modified test.py

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ for i in range(rows):
   for j in range(cols):
     dataset[i, j] = np.random.randint(1, 101)
 
-df = pd.DataFrame(dataset)
+df = pd.DataFrame(dataset, columns=[chr(i + 65) for i in range(cols)])
 
 print(df)
 
