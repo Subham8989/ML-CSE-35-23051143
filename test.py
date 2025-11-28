0780cdcdc678337636d4649be7ebdacc06478de9
Subham8989 <srigurujay1@gmail.com>
2025-11-28 08:56:34 +0530

    Modified test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+rows = int(input("Enter the number of rows: "))
+cols = int(input("Enter the number of columns: "))
+
+dataset = np.zeros((rows, cols), dtype=int)
+
+for i in range(rows):
+  for j in range(cols):
+    dataset[i, j] = np.random.randint(1, 101)
+
+df = pd.DataFrame(dataset)
+
+print(df)
+
+df.to_csv("data.csv")
