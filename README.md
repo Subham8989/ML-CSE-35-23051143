ac7d4a256ea344aa7bd9808b954c43b0e192acd7
Subham8989 <srigurujay1@gmail.com>
2025-12-02 23:12:27 +0530

    Collected Data using Web Scraping

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This is a repository of ML course where all codes will be pasted.
 
--- `test.py` -> generation of csv with random numbers
+The contents are as follows:
+
+1. `test.py` -> Generation of `data.csv` with __random numbers__.
---
+++ task2.py
@@ -0,0 +1,30 @@
+# WEB SCRAPING DATA
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+scrapping_url = "https://books.toscrape.com/"
+
+res = requests.get(scrapping_url)
+soup = BeautifulSoup(res.text, "html.parser")
+
+titles = []
+prices = []
+
+products = soup.select(".product_pod")
+
+for p in products:
+  title = p.h3.a["title"]
+  price = p.select_one(".price_color").text.strip()
+
+  titles.append(title)
+  prices.append(price)
+
+df = pd.DataFrame({
+  "title": titles,
+  "price": prices
+})
+
+df.to_csv("books_dataset.csv", index=False)
+print(df.head())
